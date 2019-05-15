953ad0992052be5c7267d935239438959a3b712d
Gilang Sinawang <giirugiru@gmail.com>
2019-05-15 08:28:23 +0700

    Second update

--- OOP/ViewController.swift
+++ OOP/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print ("My first commit boi")
+        print ("My second update on github")
     }
 
 
