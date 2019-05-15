3fc6176e0a3a95334aaf7e7a1842aa435768fb42
Gilang Sinawang <giirugiru@gmail.com>
2019-05-15 08:18:05 +0700

    New print function

---
+++ OOP/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  OOP
+//
+//  Created by Gilang Sinawang on 15/05/19.
+//  Copyright © 2019 Gilang Sinawang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print ("My first commit boi")
+    }
+
+
+}
+
